d9a903ec9760dcf1026875520fc59c0c4bb335f3
AndriiMartishyn <contactgently@gmail.com>
2024-12-26 12:21:11 +0200

    Initial structure commit

---
+++ mock-resource-server/src/main/java/com/martishyn/mockresourceserver/MockResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.martishyn.mockresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockResourceServerApplication.class, args);
+	}
+
+}
---
+++ mock-resource-server/src/test/java/com/martishyn/mockresourceserver/MockResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martishyn.mockresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
