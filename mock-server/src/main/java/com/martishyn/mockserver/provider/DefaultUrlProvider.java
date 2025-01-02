f92756cf11386b7d76bb7cb13a076d37f2ddc215
AndriiMartishyn <contactgently@gmail.com>
2025-01-02 13:29:05 +0200

    Refactored code to make data streamed in chunks. Added dataset with 100000 records as JSON file

--- mock-server/src/main/java/com/martishyn/mockserver/provider/DefaultUrlProvider.java
+++ mock-server/src/main/java/com/martishyn/mockserver/provider/DefaultUrlProvider.java
@@ -2,13 +2,20 @@ package com.martishyn.mockserver.provider;
 
 import org.springframework.stereotype.Component;
 
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Component
 public class DefaultUrlProvider implements UrlProvider {
 
     @Override
-    public URL getUrlFromFile(String fileName, Class<?> clazz) {
-        return clazz.getClassLoader().getResource(fileName);
+    public Path getUrlFromFile(String fileName, Class<?> clazz) {
+        try {
+            return Paths.get(clazz.getClassLoader().getResource(fileName).toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
